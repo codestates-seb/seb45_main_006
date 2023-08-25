df68a421ddacf4a57119d5a8b6d97221bf9bb663
origin <git@github.com:ehdals0405/PreProjectback.git>
2023-08-25 11:31:49 +0900

    [BE]:tada:Backend Project Setting

---
+++ server/DevSquad/src/test/java/WOOMOOL/DevSquad/DevSquadApplicationTests.java
@@ -0,0 +1,13 @@
+package WOOMOOL.DevSquad;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevSquadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
