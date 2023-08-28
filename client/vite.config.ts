8b3b3dde52d4c11a12a785728df44b0befe83db4
kay0829 <eluka97@daum.net>
2023-08-28 10:10:54 +0900

    [FE] 🔧 1) 기본적으로 필요한 절대경로 설정

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -4,4 +4,15 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react()],
+    resolve: {
+        alias: [
+            { find: "@api", replacement: "/src/api" },
+            { find: "@component", replacement: "/src/component" },
+            { find: "@container", replacement: "/src/container" },
+            { find: "@feature", replacement: "/src/feature" },
+            { find: "@hook", replacement: "/src/hook" },
+            { find: "@util", replacement: "/src/util" },
+            { find: "@", replacement: "/src" },
+        ],
+    },
 });
