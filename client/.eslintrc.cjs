99e175e62182eb4712138544cd8199d8bb0fedd0
kay0829 <eluka97@daum.net>
2023-08-29 14:02:01 +0900

    [FE] 📦 1) eslint-plugin-tailwindcss eslint-plugin-prettier 라이브러리 추가 및 설정

--- client/.eslintrc.cjs
+++ client/.eslintrc.cjs
@@ -6,12 +6,24 @@ module.exports = {
         "plugin:@typescript-eslint/recommended",
         "plugin:react-hooks/recommended",
         "prettier",
-        "plugin:prettier/recommende",
+        "plugin:prettier/recommended",
+        "plugin:tailwindcss/recommended",
     ],
+    settings: {
+        tailwindcss: {
+            callees: ["cls", "classnames", "clsx", "ctl"],
+            classRegex: "^(class(Name)?|\\w+Style)$",
+            config: "tailwind.config.js",
+            cssFiles: "./src/**/*.css",
+        },
+    },
     ignorePatterns: ["dist", ".eslintrc.cjs"],
     parser: "@typescript-eslint/parser",
     plugins: ["react-refresh"],
     rules: {
         "react-refresh/only-export-components": ["warn", { allowConstantExport: true }],
+        "tailwindcss/classnames-order": "warn",
+        "tailwindcss/no-custom-classname": "warn",
+        "tailwindcss/no-contradicting-classname": "error",
     },
 };
