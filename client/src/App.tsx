2f30429c1ff0b60d92755a29de8bf979a80a3553
kay0829 <eluka97@daum.net>
2023-09-01 03:01:43 +0900

    [FE] 🔧 21) react - query 세팅

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,6 +2,9 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import { RecoilRoot } from "recoil";
 
 import Main from "@container/Main";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+
 import ErrorPage from "@container/ErrorPage";
 import MyPage from "@container/MyPage";
 
@@ -22,12 +25,17 @@ const router = createBrowserRouter([
     },
 ]);
 
+const queryClient = new QueryClient();
+
 function App() {
     return (
-        <div className="m-0 box-border flex justify-center p-0 font-sans">
-            <RecoilRoot>
-                <RouterProvider router={router} />
-            </RecoilRoot>
+        <div className="m-0 box-border flex justify-center p-0 font-sans text-primary">
+            <QueryClientProvider client={queryClient}>
+                <RecoilRoot>
+                    <RouterProvider router={router} />
+                </RecoilRoot>
+                <ReactQueryDevtools initialIsOpen={false} />
+            </QueryClientProvider>
         </div>
     );
 }
