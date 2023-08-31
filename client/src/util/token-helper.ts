bee30ea7f3d80f6d365cca2e4195f30cdbcc44c0
kay0829 <eluka97@daum.net>
2023-09-01 02:35:45 +0900

    [FE] 🧑‍💻 21) localstorage-helper, token-helper, pagination-helper 유틸 함수 추가

---
+++ client/src/util/token-helper.ts
@@ -0,0 +1,31 @@
+import dayjs from "dayjs";
+import jwt_decode from "jwt-decode";
+
+// TODO jwt payload 확인 후 수정
+type ACCESS_TOKEN = {
+    userId: number;
+    nickname: string;
+    email: string;
+    iat: Date;
+    exp: Date;
+};
+
+/**
+ * token을 파싱하는 함수
+ * @param {string} token access token 또는 refresh token
+ * @returns {ACCESS_TOKEN} 토큰을 파싱한 객체
+ */
+export const parseJwt = (token: string): ACCESS_TOKEN => {
+    const decoded: ACCESS_TOKEN = jwt_decode(token);
+    return decoded;
+};
+
+/**
+ * 토큰 만료 시간과 현재 시간을 비교하여 유효한 토큰인지 검증하는 함수
+ * @param {string} token access token 또는 refresh token
+ * @returns {boolean} 토큰 유효 여부
+ */
+export const isValidToken = (token: string): boolean => {
+    const tokenJwtData = parseJwt(token);
+    return dayjs(tokenJwtData.exp) > dayjs();
+};
