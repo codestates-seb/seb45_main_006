34f31110e7f57f167ff8fa5f3878b0bed290579d
kay0829 <eluka97@daum.net>
2023-09-04 12:17:54 +0900

    [FE] 🔧 49) QueryClient 기본 설정 추가 및 전역 에러 처리 설정 추가

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,8 +1,9 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import { RecoilRoot } from "recoil";
 
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { QueryClient, QueryClientProvider, QueryCache } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { isAxiosError } from "axios";
 
 import Container from "@container/Container";
 import ErrorPage from "@container/ErrorPage";
@@ -50,7 +51,26 @@ const router = createBrowserRouter([
     },
 ]);
 
-const queryClient = new QueryClient();
+const queryClient = new QueryClient({
+    defaultOptions: {
+        queries: {
+            refetchOnMount: false,
+            refetchOnReconnect: false,
+            refetchOnWindowFocus: false,
+            retry: 0,
+        },
+    },
+    queryCache: new QueryCache({
+        onError: (error) => {
+            if (isAxiosError(error)) {
+                if (error.response?.status === 500) {
+                    // 서버 500 에러
+                }
+                // 토큰 만료 오류 체크
+            }
+        },
+    }),
+});
 
 function App() {
     return (
