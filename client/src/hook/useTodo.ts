80a96f1cc2546876e2932889df9bff47420ca1f6
kay0829 <eluka97@daum.net>
2023-09-05 15:15:10 +0900

    [FE] 🧑‍💻 71) useToast hook 추가 - 토스트를 추가하는 createToast - 토스트를 삭제하는 removeToast - 2.5초 후 자동으로 토스트가 사라지는 fireToast

---
+++ client/src/hook/useTodo.ts
@@ -0,0 +1,56 @@
+import { useRecoilValue, useSetRecoilState } from "recoil";
+import { toastState, addToastItem, removeToastItem } from "@feature/Global";
+import { getRandomID } from "@util/random-helper";
+
+import { IToast } from "@type/common";
+
+export const useToast = () => {
+    const toasts = useRecoilValue(toastState);
+    const id = getRandomID();
+    const addToastItemFn = useSetRecoilState(addToastItem);
+    const removeToastItemFn = useSetRecoilState(removeToastItem);
+
+    const fireToast = (toast: IToast) => {
+        addToastItemFn([
+            {
+                id: id,
+                content: toast.content || "",
+                isConfirm: false,
+            },
+        ]);
+
+        setTimeout(() => {
+            removeToastItemFn([
+                {
+                    id: id,
+                    content: toast.content || "",
+                    isConfirm: false,
+                },
+            ]);
+        }, 2500);
+    };
+
+    const createToast = (toast: IToast) => {
+        addToastItemFn([
+            {
+                id: id,
+                content: toast.content || "",
+                isConfirm: toast.isConfirm || false,
+                callback: toast.callback,
+            },
+        ]);
+    };
+
+    const removeToast = (toast: IToast) => {
+        removeToastItemFn([
+            {
+                id: toast.id,
+                content: toast.content || "",
+                isConfirm: toast.isConfirm || false,
+                callback: toast.callback,
+            },
+        ]);
+    };
+
+    return { toasts, createToast, removeToast, fireToast };
+};
