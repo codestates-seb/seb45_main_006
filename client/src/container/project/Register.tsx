813396d8ee35b93bf5d90bbb02e4f695afb7d1c8
kay0829 <eluka97@daum.net>
2023-09-06 20:23:35 +0900

    [FE] 🐛 76) 등록 페이지 관련 에러 및 불필요한 css 삭제

--- client/src/container/project/Register.tsx
+++ client/src/container/project/Register.tsx
@@ -38,62 +38,65 @@ export default function Register() {
     //     navigate("/todos/register");
     // }
     return (
-        <div className="m-80 flex items-center justify-center">
-            <div className="flex w-11/12 flex-col items-center justify-center rounded-lg bg-project">
-                <Typography type="Heading" text="어떤 프로젝트인가요?" styles="pt-60 pb-30 pl-60 self-baseline" />
-                <BoardInput
-                    name="projectName"
-                    label="프로젝트명"
-                    required={true}
-                    placeholder="ex) 카메라 서비스 개발"
-                    value={inputs.projectName}
-                    onChange={handleInput}
-                    maxlength={20}
-                />
-                <BoardTextarea
-                    type="FIELD"
-                    name="detail"
-                    label="프로젝트 상세내용"
-                    required={true}
-                    placeholder="ex) 카메라 서비스 개발"
-                    value={inputs.detail}
-                    onChange={handleInput}
-                />
-                <BoardInput
-                    name="stack"
-                    label="요구스택"
-                    required={true}
-                    placeholder="ex) java, javascript"
-                    value={inputs.stack}
-                    onChange={handleInput}
-                />
-                <BoardInput label="모집여부" disabled={true} placeholder="모집중" onChange={handleInput} />
-                <BoardInput
-                    name="date"
-                    label="프로젝트 기간"
-                    required={true}
-                    placeholder="ex) 2023-09-22"
-                    value={inputs.date}
-                    onChange={handleInput}
-                />
-                <BoardInput
-                    name="group"
-                    label="모집인원"
-                    required={true}
-                    placeholder="ex) 6명"
-                    value={inputs.group}
-                    onChange={handleInput}
-                />
-                <Button
-                    type="PROJECT_POINT"
-                    styles="mb-20"
-                    isFullBtn={false}
-                    onClickHandler={() => {
-                        navigate("/projects/:projectBoardId");
-                    }}
-                >
-                    <Typography text="등록하기" type="Label" color="text-white" />
-                </Button>
+        <div className="m-80">
+            <div className="flex w-11/12 justify-center rounded-lg bg-project">
+                <div className="flex w-11/12 flex-col">
+                    <Typography type="Heading" text="어떤 프로젝트인가요?" styles="pt-60 pb-30 pl-10 self-baseline" />
+                    <BoardInput
+                        name="projectName"
+                        label="프로젝트명"
+                        required={true}
+                        placeholder="ex) 카메라 서비스 개발"
+                        value={inputs.projectName}
+                        onChange={handleInput}
+                        maxlength={20}
+                    />
+                    <BoardTextarea
+                        name="detail"
+                        label="프로젝트 상세내용"
+                        required={true}
+                        placeholder="ex) 카메라 서비스 개발"
+                        value={inputs.detail}
+                        onChange={handleInput}
+                    />
+                    <BoardInput
+                        name="stack"
+                        label="요구스택"
+                        required={true}
+                        placeholder="ex) java, javascript"
+                        value={inputs.stack}
+                        onChange={handleInput}
+                    />
+                    <BoardInput label="모집여부" disabled={true} placeholder="모집중" onChange={handleInput} />
+                    <BoardInput
+                        name="date"
+                        label="프로젝트 기간"
+                        required={true}
+                        placeholder="ex) 2023-09-22"
+                        value={inputs.date}
+                        onChange={handleInput}
+                    />
+                    <BoardInput
+                        name="group"
+                        label="모집인원"
+                        required={true}
+                        placeholder="ex) 6명"
+                        value={inputs.group}
+                        onChange={handleInput}
+                    />
+                    <div className="flex w-full justify-center">
+                        <Button
+                            type="PROJECT_POINT"
+                            styles="mb-20"
+                            isFullBtn={false}
+                            onClickHandler={() => {
+                                navigate("/projects/:projectBoardId");
+                            }}
+                        >
+                            <Typography text="등록하기" type="Label" color="text-white" />
+                        </Button>
+                    </div>
+                </div>
             </div>
         </div>
     );
