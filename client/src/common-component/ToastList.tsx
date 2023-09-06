abbe8dcbb15a07e759dde68ae69d7d0f98974528
kay0829 <eluka97@daum.net>
2023-09-06 09:39:36 +0900

    [FE] 🔥 75) 불필요한 console.log 삭제

--- client/src/common-component/ToastList.tsx
+++ client/src/common-component/ToastList.tsx
@@ -5,7 +5,6 @@ import Toast from "@component/Toast";
 
 function ToastList() {
     const toasts = useRecoilValue(toastState);
-    console.log(toasts);
 
     return (
         <div className="fixed left-2/4 top-120 -translate-x-2/4">
