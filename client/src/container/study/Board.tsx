9341f47b6358c7822de803453bc315b688758271
LEE YEJIN <dldulwls@gmail.com>
2023-09-10 15:55:00 +0900

    [FE] 🔧 57) 스터디 일부 코드 수정

--- client/src/container/study/Board.tsx
+++ client/src/container/study/Board.tsx
@@ -1,12 +1,12 @@
 import { useState } from "react";
-
 import Button from "@component/Button";
 import Typography from "@component/Typography";
 import { useNavigate } from "react-router-dom";
-import BoardList from "@container/project/component/BoardList";
-import Toggle from "@container/project/component/Toggle";
+import BoardList from "@container/study/component/BoardList";
+import Toggle from "@component/project-study/Toggle";
 import CommonSearchFilters from "@component/board/SearchFilters";
 import SearchInput from "@component/board/SearchInput";
+import { useGetAllStudies } from "@api/study/hook";
 
 const Board = () => {
     const navigate = useNavigate();
@@ -15,6 +15,7 @@ const Board = () => {
     // 스택, 정렬 방식 필터
     const [selectedStacks, setSelectedStacks] = useState<Array<string>>([]);
     const [selectedOrder, setSelectedOrder] = useState<Array<string>>([]);
+    const { data: studiesList } = useGetAllStudies();
 
     const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
         setSearchValue(e.currentTarget.value);
@@ -52,8 +53,7 @@ const Board = () => {
                 </div>
                 <Toggle />
             </div>
-            <BoardList />
-            <BoardList />
+            <> {Array.isArray(studiesList) && studiesList.map((v) => <BoardList study={v} />)}</>
         </div>
     );
 };
