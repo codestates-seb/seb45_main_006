25e2ec045420f027d5c1608d44f5d0a6a3a76def
kay0829 <eluka97@daum.net>
2023-09-11 17:14:22 +0900

    [FE] 💄 99) 마이페이지 - 차단 관리 컴포넌트 추가

---
+++ client/src/container/mypage/component/UserBlock.tsx
@@ -0,0 +1,43 @@
+import { useGetMemberDetail, useDeleteBlockMember } from "@api/member/hook";
+import Typography from "@component/Typography";
+
+const UserBlockItem = ({ memberId }: { memberId: number }) => {
+    const linkCss = "bg-tertiary px-8 py-4 hover:bg-light hover:font-bold";
+
+    const { data: blockUser } = useGetMemberDetail({ memberId });
+    const { mutate: deleteBlockMember } = useDeleteBlockMember();
+
+    return (
+        <div className="flex items-center p-10">
+            {blockUser && (
+                <>
+                    <div className="flex w-9/12">
+                        <div className="mr-8 h-36 w-36 overflow-hidden rounded border-1 border-borderline">
+                            <img src={blockUser.profilePicture} alt="" />
+                        </div>
+                        <Typography text={blockUser.nickname} type="Body" />
+                    </div>
+                    <div className="flex w-3/12 justify-center">
+                        <button className={linkCss} onClick={() => deleteBlockMember({ memberId })}>
+                            <Typography text="차단 해제" type="Body" />
+                        </button>
+                    </div>
+                </>
+            )}
+        </div>
+    );
+};
+
+function UserBlock({ blockList }: { blockList: Array<number> }) {
+    return (
+        <div className="flex flex-1 flex-col items-center rounded-md bg-white p-12 shadow-md">
+            <div className="w-full">
+                {blockList.map((v) => (
+                    <UserBlockItem key={`block-${v}`} memberId={v} />
+                ))}
+            </div>
+        </div>
+    );
+}
+
+export default UserBlock;
