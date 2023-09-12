eef79a545f35d395ce36e87ccdbc21b32fe391cc
kay0829 <eluka97@daum.net>
2023-09-12 20:19:04 +0900

    [FE] 👔 131) commonApi 로 호출 시에 accessToken 있으면 포함하도록 수정 - 리스트 조회 시 토큰 있을 경우 차단 유저 필터 / 토큰 없을 경우 모든 데이터 조회

--- client/src/api/common/commonApi.ts
+++ client/src/api/common/commonApi.ts
@@ -1,4 +1,5 @@
 import axios from "axios";
+import { getItemFromStorage } from "@util/localstorage-helper";
 
 let apiEndpoint = "";
 if (import.meta.env.VITE_APP_API_ENDPOINT && typeof import.meta.env.VITE_APP_API_ENDPOINT === "string") {
@@ -11,4 +12,14 @@ export const commonApi = axios.create({
         "Content-Type": "application/json",
         "ngrok-skip-browser-warning": "69420",
     },
+});
+
+commonApi.interceptors.request.use(async (config) => {
+    const accessToken = getItemFromStorage("accessToken") || "";
+    if (accessToken) {
+        config.headers.Authorization = `Bearer ${accessToken}`;
+        return config;
+    }
+
+    return config;
 });
