7952d009063d380f59611d31ca1fc9107928e1a8
kay0829 <eluka97@daum.net>
2023-09-12 13:50:16 +0900

    [FE] 💄 121) 채팅방 리스트, 채팅방 컴포넌트 추가 (미완)

---
+++ client/src/container/chat/component/ChatRoomItem.tsx
@@ -0,0 +1,18 @@
+import { useRecoilValue } from "recoil";
+import { chatRoomIdAtom } from "@feature/chat";
+
+import { useGetEnrollChatRoom } from "@api/chat/hook";
+
+function ChatRoomItem() {
+    const chatRoomId = useRecoilValue(chatRoomIdAtom);
+
+    // TODO: 웹소켓 connect
+    // 오른쪽 클릭 삭제
+
+    const { data: chatMessages } = useGetEnrollChatRoom({ chatRoomId });
+    console.log(chatMessages);
+
+    return <div>뇨뇨</div>;
+}
+
+export default ChatRoomItem;
