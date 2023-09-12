be597e7e4f735953d5a6b2f2155700db38cc15bd
LEE YEJIN <dldulwls@gmail.com>
2023-09-12 13:44:03 +0900

    [FE] 🔧 58) 프로젝트, 스터디 /edit 경로 추가

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -21,7 +21,7 @@ import StudyRegister from "@container/study/Register";
 import SignUp1 from "@container/sign/SignUp1";
 import SignUp2 from "@container/sign/SignUp2";
 import SignUp3 from "@container/sign/SignUp3";
-import SetPro from "@container/sign/setProfile";
+// import SetPro from "@container/sign/setProfile";
 import TempProfile1 from "@container/sign/TempProfile1";
 import TempProfile4 from "@container/sign/TempProfile4";
 import FindPw from "@container/sign/FindPw";
@@ -55,6 +55,10 @@ const router = createBrowserRouter([
                 element: <ProjectRegister />,
             },
             {
+                path: "/projects/:projectBoardId/edit",
+                element: <ProjectRegister />,
+            },
+            {
                 path: "/projects/:projectBoardId",
                 element: <ProjectDetails />,
             },
@@ -64,6 +68,10 @@ const router = createBrowserRouter([
             },
             {
                 path: "/studies/register",
+                element: <StudyRegister />,
+            },
+            {
+                path: "/studies/:studyBoardId/edit",
                 element: <StudyRegister />,
             },
             {
@@ -86,10 +94,10 @@ const router = createBrowserRouter([
                 path: "/signup/3",
                 element: <SignUp3 />,
             },
-            {
-                path: "/setpro",
-                element: <SetPro />,
-            },
+            // {
+            //     path: "/setpro",
+            //     element: <SetPro />,
+            // },
             {
                 path: "/signup/1",
                 element: <SignUp1 />,
