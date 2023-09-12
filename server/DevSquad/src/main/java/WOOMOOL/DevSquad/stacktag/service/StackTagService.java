a9aaf94e6d00fd192323ba764e1d64b766f4847a
seoljuyeon <sxxljxxxxn@gmail.com>
2023-09-12 17:59:26 +0900

    [BE] :sparkles: 6) Project 에 Stack 추가 중

--- server/DevSquad/src/main/java/WOOMOOL/DevSquad/stacktag/service/StackTagService.java
+++ server/DevSquad/src/main/java/WOOMOOL/DevSquad/stacktag/service/StackTagService.java
@@ -11,10 +11,7 @@ import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Optional;
-import java.util.Set;
+import java.util.*;
 import java.util.stream.Collectors;
 @Service
 public class StackTagService {
@@ -37,18 +34,17 @@ public class StackTagService {
         }
     }
 
-    public void createProjectStackTag(Project project, Set<String> stackTaglist){
+    public Set<StackTag> createProjectStackTag(Set<String> stackTaglist){
+        if( stackTaglist.isEmpty() ) return null;
 
-        // 수정시 스택 객체 초기화
-        project.getStackTags().clear();
+        Set<StackTag> stackTags = new HashSet<>();
 
-        if(stackTaglist.size() > 0) {
-            for (String stackTags : stackTaglist) {
-                StackTag stackTag = stackTagRepository.findByTagName(stackTags);
-                stackTag.getProjectBoardList().add(project);
-                project.getStackTags().add(stackTag);
-            }
+        for(String stack : stackTaglist) {
+            StackTag stackTag = stackTagRepository.findByTagName(stack);
+            stackTags.add(stackTag);
         }
+
+        return stackTags;
     }
 
     //검색어가 있을시 스택태그검색과 없을시 검색
--- server/DevSquad/src/main/java/WOOMOOL/DevSquad/studyboard/dto/StudyDto.java
+++ server/DevSquad/src/main/java/WOOMOOL/DevSquad/studyboard/dto/StudyDto.java
@@ -3,6 +3,7 @@ package WOOMOOL.DevSquad.studyboard.dto;
 import WOOMOOL.DevSquad.comment.dto.CommentDto;
 import WOOMOOL.DevSquad.member.dto.MemberProfileDto;
 import WOOMOOL.DevSquad.studyboard.entity.Study;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -10,6 +11,7 @@ import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Positive;
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Set;
 
 public class StudyDto {
     @Getter
@@ -20,12 +22,17 @@ public class StudyDto {
         @NotBlank(message = "내용을 작성해 주세요.")
         private String content;
 
+//        @NotNull
+        private Set<String> stack;
+
         @Positive(message = "모집 인원을 작성해 주세요.")
         private Integer recruitNum;
 
     }
 
-    @Getter @Setter
+    @Getter
+    @Setter
+    @AllArgsConstructor
     public static class PatchDto {
         private Long boardId;
 
@@ -35,13 +42,18 @@ public class StudyDto {
 //        @NotBlank(message = "내용을 작성해 주세요.")
         private String content;
 
+//        @NotNull
+        private Set<String> stack;
+
 //        @Positive(message = "모집 인원을 작성해 주세요.")
         private Integer recruitNum;
 
         private Study.StudyStatus studyStatus;
     }
 
-    @Getter @Setter
+    @Getter
+    @Setter
+    @AllArgsConstructor
     public static class previewResponseDto {
         private Long boardId;
         private String title;
@@ -51,20 +63,24 @@ public class StudyDto {
         private Study.StudyStatus studyStatus;
         private int viewCount;
         private MemberProfileDto.listResponse memberProfile;
+        private Set<String> stacks;
     }
 
-    @Getter @Setter
+    @Getter
+    @Setter
+    @AllArgsConstructor
     public static class AllResponseDto {
         private Long boardId;
         private String title;
         private String content;
+        private Set<String> stacks;
         private LocalDateTime createdAt;
         private LocalDateTime modifiedAt;
         private Integer recruitNum;
         private Study.StudyStatus studyStatus;
         private boolean bookmarked;
         private int viewCount;
-        private List<CommentDto.Response> commentList;
+//        private List<CommentDto.Response> commentList;
         private MemberProfileDto.listResponse memberProfile;
     }
 }
