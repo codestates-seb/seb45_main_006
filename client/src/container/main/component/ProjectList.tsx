b4102df76f8be2f9e3516382ae2040c12c60a2c4
LEE YEJIN <dldulwls@gmail.com>
2023-09-13 14:26:34 +0900

    [FE] ♻️ 59) 프로젝트 전체 조회 (페이지네이션 X) 수정

--- client/src/container/main/component/ProjectList.tsx
+++ client/src/container/main/component/ProjectList.tsx
@@ -1,20 +1,35 @@
+// import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import dayjs from "dayjs";
+// import Pagination from "@component/Pagination";
 import Typography from "@component/Typography";
 import Tag from "@component/project-study/Tag";
 import { CommonResProjects } from "@type/project/project.res.dto";
-import dayjs from "dayjs";
-import { useNavigate } from "react-router-dom";
+// import { useGetAllProjects } from "@api/project/hook";
 
 const ProjectList = ({ project }: { project: CommonResProjects }) => {
+    // 페이지 필터
+    // const [curPage, setCurPage] = useState<number>(1);
+    // const [totalItems, setTotalItems] = useState<number>(0);
+
+    // const { data: project } = useGetAllProjects({ boardId, page: curPage });
+
     const navigate = useNavigate();
 
     const handleProjectItemClick = () => {
         navigate("/projects/:projectBoardId", { state: project.boardId });
     };
 
+    // useEffect(() => {
+    //     if (project?.pageInfo.totalElements) {
+    //         setTotalItems(project?.pageInfo.totalElements);
+    //     }
+    // }, [project?.pageInfo.totalElements]);
+
     return (
         <div
             onClick={handleProjectItemClick}
-            className="m-10 flex h-300 w-1/4 flex-col justify-between rounded-lg border-2 border-solid border-project p-20 shadow-lg transition-transform hover:scale-105 hover:bg-gray-100"
+            className="m-10 flex h-300 w-260 flex-col justify-between rounded-lg border-2 border-solid border-project p-20 shadow-lg transition-transform hover:scale-105 hover:bg-gray-100"
         >
             <div>
                 <div className="flex w-48 items-center justify-center rounded bg-deadline ">
@@ -36,6 +51,7 @@ const ProjectList = ({ project }: { project: CommonResProjects }) => {
                 <div className="h-40 w-40 rounded-3xl bg-deadline">유저</div>
                 <div className="mx-20 text-20">yeeendy</div>
             </div>
+            {/* <Pagination curPage={curPage} setCurPage={setCurPage} totalItems={totalItems || 0} /> */}
         </div>
     );
 };
