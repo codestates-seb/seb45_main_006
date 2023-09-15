80ebc1dd0b8fd96beacbeb7fb16f965ee5ecec59
origin <git@github.com:ehdals0405/PreProjectback.git>
2023-09-15 10:51:21 +0900

    [BE] :recycle: 환경변수 처리

--- server/DevSquad/src/main/java/WOOMOOL/DevSquad/auth/userdetails/MemberAuthority.java
+++ server/DevSquad/src/main/java/WOOMOOL/DevSquad/auth/userdetails/MemberAuthority.java
@@ -1,5 +1,6 @@
 package WOOMOOL.DevSquad.auth.userdetails;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.stereotype.Component;
@@ -12,7 +13,8 @@ public class MemberAuthority {
     private final List<String> USER_ROLES = List.of("USER");
     private final List<String> ADMIN_ROLES = List.of("USER", "ADMIN");
 
-    private final String admin = "admin@gmailcom";
+    @Value("admin.mail")
+    private String admin;
 
 
     // admin 계정 정보는 환경 변수로 빼기
