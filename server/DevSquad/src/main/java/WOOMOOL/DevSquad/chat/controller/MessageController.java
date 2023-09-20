ee0f1beadf162851be1583eac99dc850c2393b65
origin <git@github.com:ehdals0405/PreProjectback.git>
2023-09-20 12:08:27 +0900

    [BE] :recycle: 닉네임 10자로 확장

--- server/DevSquad/src/main/java/WOOMOOL/DevSquad/chat/controller/MessageController.java
+++ server/DevSquad/src/main/java/WOOMOOL/DevSquad/chat/controller/MessageController.java
@@ -8,6 +8,7 @@ import WOOMOOL.DevSquad.chat.mapper.MessageMapper;
 import WOOMOOL.DevSquad.chat.service.ChatRoomService;
 import WOOMOOL.DevSquad.chat.service.MessageService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.messaging.handler.annotation.DestinationVariable;
@@ -20,6 +21,7 @@ import java.time.LocalDateTime;
 
 @RestController
 @RequiredArgsConstructor
+@Slf4j
 public class MessageController {
 
     private final MessageMapper messageMapper;
